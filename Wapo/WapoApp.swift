d3223b4507c2866a02201db8aa4a41726efce729
Artem Chmylenko <achmilenko58@gmail.com>
2026-03-28 14:41:03 +0200

    initial commit

---
+++ Wapo/WapoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WapoApp.swift
+//  Wapo
+//
+//  Created by Artem Chmylenko on 28.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct WapoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
